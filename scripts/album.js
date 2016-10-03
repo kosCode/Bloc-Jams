6cc33af3960d12fb3dc9e8defd23a6442abcddf5
Kevin Kosiorek <Kevin Kosiorek>
2016-10-03 18:05:23 -0400

    assignment-19-jQueryNextPrev

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,15 @@
+var setSong = function(songNumber){
+  currentlyPlayingSongNumber = parseInt(songNumber);
+  currentSongFromAlbum = currentAlbum.songs[songNumber-1];
+};
+
+var getSongNumberCell = function(number) {
+  return $('.song-item-number[data-song-number="' + number +'"]');
+};
+// var getSongNumberCell = functon(number){
+//   return $('.song-item-number[data-song-number="' + number +'"]');
+// };
+
 var createSongRow = function(songNumber, songName, songLength){
     var template =
         '<tr class="album-view-song-item">'
@@ -18,15 +30,13 @@ var createSongRow = function(songNumber, songName, songLength){
     	}
     	if (currentlyPlayingSongNumber !== songNumber) {
     		$(this).html(pauseButtonTemplate);
-    		currentlyPlayingSongNumber = songNumber;
-        currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+        setSong(songNumber);
         updatePlayerBarSong();
     	} else if (currentlyPlayingSongNumber === songNumber) {
     		// Switch from Pause -> Play button to pause currently playing song.
     		$(this).html(playButtonTemplate);
         $('.main-controls .play-pause').html(playerBarPlayButton);
-    		currentlyPlayingSongNumber = null;
-        currentSongFromAlbum = null;
+    		setSong(null);
     	}
     };
 
